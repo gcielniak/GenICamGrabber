a56072363638167002ab7d3e309052c3beb1cb10
gcielniak <gcielniak@lincoln.ac.uk>
2017-01-08 22:35:07 +0000

    partial GenIAPI support

--- include/GenICamera.h
+++ include/GenICamera.h
@@ -2,7 +2,11 @@
 #include <boost/thread.hpp>
 #include <iCVCDriver.h>
 #include <iCVCUtilities.h>
+#include <iCVGenApi.h>
+#include <CVCError.h>
 #include "Module.h"
+
+using namespace std;
 
 class GenICamera : public Module {
 protected:
@@ -64,6 +68,139 @@ public:
 				throw LibException("GenICamera::Init, Error loading \"" + std::string(driverPath) + "\" driver!");
 			initialised = true;
 		}
+	}
+
+	void NodeList() {
+		char name[256] = { 0 };
+		size_t name_size = sizeof(name);
+		NODEMAP hNodeMap;
+		NMHGetNodeMap(hCamera, hNodeMap);
+		cvbdim_t count;
+		NMNodeCount(hNodeMap, count);
+		NMListNode(hNodeMap, 1, name, name_size);
+		cerr << count << endl;
+		cerr << name << endl;
+	}
+
+	void NodeInfo(NODE node) {
+		TNodeType node_type;
+		char name[256] = { 0 };
+		size_t name_length = sizeof(name);
+
+		NType(node, node_type);
+
+		if (node_type == NT_EnumEntry)
+			return;
+
+		NInfoAsString(node, ::TNodeInfo::NI_Name, name, name_length);
+		cerr << name << ": ";
+
+		NInfoAsString(node, ::TNodeInfo::NI_DisplayName, name, name_length);
+		cerr << name << ": ";
+
+		cvbbool_t bvalue;
+		double fvalue;
+		cvbint64_t ivalue;
+
+		switch (node_type) {
+		case NT_Boolean:
+			NGetAsBoolean(node, bvalue);
+			cerr << bvalue << endl;
+			break;
+		case NT_Command:
+			cerr << "command" << endl;
+			break;
+		case NT_Category:
+			cerr << "category" << endl;
+			break;
+		case NT_Float:
+			NGetAsFloat(node, fvalue);
+			cerr << fvalue << endl;
+			break;
+		case NT_Integer:
+			NGetAsInteger(node, ivalue);
+			cerr << ivalue << endl;
+			break;
+		case NT_String:
+			NGetAsString(node, name, name_length);
+			cerr << name << endl;
+			break;
+		case NT_EnumEntry:
+			//ignore as these will appear in Enumeration
+			break;
+		case NT_Enumeration:
+			cvbint64_t num_entries;
+			NInfoAsInteger(node, ::TNodeInfo::NI_NumEnumEntries, num_entries);
+			for (cvbdim_t i = 0; i < num_entries; i++) {
+				NInfoEnum(node, i, ivalue, name, name_length);
+				cerr << name << " (" << ivalue << "), ";
+			}
+			cerr << endl;
+			break;
+		default:
+			cerr << "value type " << node_type << endl;
+			break;
+		}
+	}
+
+	void genicam_access() {
+		cerr << "----------------" << endl;
+		NodeList();
+		cerr << "----------------" << endl;
+		NODEMAP hNodeMap = NULL;
+		cvbres_t result = NMHGetNodeMap(hCamera, hNodeMap);
+		cvbval_t value;
+		NMNodeCount(hNodeMap, value);
+		cerr << "NMNodeCount: " << value << endl;
+
+		char name[256] = { 0 };
+		size_t name_length = sizeof(name);
+
+		for (int i = 0; i < value; i++) {
+			NMListNode(hNodeMap, i, name, name_length);
+			NODE node;
+			NMGetNode(hNodeMap, name, node);
+			NodeInfo(node);
+		}
+	}
+
+	int GetNrDevices() {
+		cvbval_t value = 0;
+
+		if (CanCameraSelect2(hCamera))
+			CS2GetNumPorts(hCamera, value);
+
+		return value;
+	}
+
+	string GetNodeValue(string node_name) {
+		NODE node;
+		NODEMAP node_map;
+		TNodeType node_type;
+		char name[256] = { 0 };
+		size_t name_size = sizeof(name);
+
+		NMHGetNodeMap(hCamera, node_map);
+		NMGetNode(node_map, node_name.c_str(), node);
+		NType(node, node_type);
+		if (node_type == NT_String)
+			NGetAsString(node, name, name_size);
+		ReleaseObject(node);
+		ReleaseObject(node_map);
+
+		return name;
+	}
+
+	string GetDeviceName(int index) {
+		stringstream device_name;
+
+		if (index < GetNrDevices()) {
+			device_name << GetNodeValue("DeviceFamilyName" );
+			device_name << " " << GetNodeValue("DeviceModelName");
+			device_name << " by " << GetNodeValue("DeviceVendorName");
+		}
+
+		return device_name.str();
 	}
 
 	void Start() {
